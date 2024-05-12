e9f434e2be532e5ef61bfb78ea07c3284b0429b1
Paul Chen <47184835+chenpaulkevin@users.noreply.github.com>
2024-05-12 11:01:54 +0800

    🎉 init(Initial Commit): Initial Commit
    Initial Commit

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,4 @@
 import { postgresAdapter } from '@payloadcms/db-postgres'
-// import { payloadCloud } from '@payloadcms/plugin-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
 import { buildConfig } from 'payload/config'
@@ -24,23 +23,15 @@ export default buildConfig({
   },
   collections: [Users, Media, Testimonials, DesignModels, Categories, Blog, Pages],
   editor: lexicalEditor({}),
-  // plugins: [payloadCloud()], // TODO: Re-enable when cloud supports 3.0
   secret: process.env.PAYLOAD_SECRET || '',
   typescript: {
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
   db: postgresAdapter({
     pool: {
-      connectionString: process.env.DATABASE_URI || '',
+      connectionString: process.env.DATABASE_URI,
     },
   }),
-
-  // Sharp is now an optional dependency -
-  // if you want to resize images, crop, set focal point, etc.
-  // make sure to install it and pass it to the config.
-
-  // This is temporary - we may make an adapter pattern
-  // for this before reaching 3.0 stable
 
   sharp,
 })
